d727eb81e3b3597f076a3f779398d7a82c4206a3
Tony Grimes <acidtone@tonygrimes.com>
2024-11-30 15:10:10 -0700

    added collapsible Monster cards, re-arranging

--- src/schemas/CustomStats.schema.ts
+++ src/schemas/CustomStats.schema.ts
@@ -1,5 +1,4 @@
 import { z } from 'astro:content';
-import { ProficiencySchema } from './Monster.schema'
 
 export const ArmorClassSchema = z.array(z.object({
   type: z.string(),
@@ -12,15 +11,7 @@ export const ArmorClassSchema = z.array(z.object({
 
 export const BattleStatsSchema = z.object({
   hit_points: z.number(),
-  armor_class:ArmorClassSchema,
-  proficiencies: z.array(ProficiencySchema),
-  damage_vulnerabilities: z.array(z.string()),
-  damage_resistances: z.array(z.string()),
-  damage_immunities: z.array(z.string()),
-  condition_immunities: z.array(z.object({
-    name: z.string(),
-    url: z.string().url(),
-  }))
+  armor_class:ArmorClassSchema
 });
 
 export const StatBlockSchema = z.object({
@@ -32,7 +23,18 @@ export const StatBlockSchema = z.object({
   charisma: z.number(),
 });
 
+export const StrengthsWeaknessesSchema = z.object({
+  damage_vulnerabilities: z.array(z.string()),
+  damage_resistances: z.array(z.string()),
+  damage_immunities: z.array(z.string()),
+  condition_immunities: z.array(z.object({
+    name: z.string(),
+    url: z.string().url(),
+  })),
+});
+
 // Type for TypeScript users
 export type ArmorClass = z.infer<typeof ArmorClassSchema>;
 export type BattleStats = z.infer<typeof BattleStatsSchema>;
 export type Stats = z.infer<typeof StatBlockSchema>;
+export type StrengthsWeaknesses = z.infer<typeof StrengthsWeaknessesSchema>;
